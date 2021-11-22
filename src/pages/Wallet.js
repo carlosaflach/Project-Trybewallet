94e9ca6006d95fb09108cb81a3d0984b2559d570
Carlos Afonso <carlos.flach15@gmail.com>
2021-11-22 16:31:14 -0300

    Implements wallet component.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import '../CSS/wallet.css';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>
-        <h1> Carteira </h1>
-      </div>
+      <header className="walletHeader">
+        <h3 data-testid="email-field">
+          Usuário:
+          {' '}
+          {email}
+        </h3>
+        <h3 data-testid="total-field">
+          Gastos Totais:
+          {' '}
+          {0}
+        </h3>
+        <h3 data-testid="header-currency-field"> BRL </h3>
+      </header>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
