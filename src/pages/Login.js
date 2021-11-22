2cd8d5e88b478ed65c0892f9f1e39f5582803004
Carlos Afonso <carlos.flach15@gmail.com>
2021-11-22 15:17:18 -0300

    Creates password input, and validate email and password.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,8 @@ class Login extends React.Component {
     super(props);
     this.state = {
       email: '',
+      password: '',
+      disabled: true,
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -13,14 +15,25 @@ class Login extends React.Component {
     const { name, value } = target;
     this.setState({
       [name]: value,
+    }, () => {
+      // get help with Renato Adorno repository => https://github.com/tryber/sd-015-a-project-trybewallet/pull/82/commits/734ed01fa7d41a92e9c0566da26a8e9b8475c985
+      const { email, password } = this.state;
+      const MIN_LENGHT = 6;
+      const emailRegex = /\S+@\S+\.\S+/; // Source: https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+      if (password.length >= MIN_LENGHT && emailRegex.test(email)) {
+        this.setState({ disabled: false });
+      } else {
+        this.setState({ disabled: true });
+      }
     });
   }
 
   render() {
-    const { email } = this.state;
+    const { email, password, disabled } = this.state;
     return (
       <div>
         <label htmlFor="emailId">
+          Email:
           <input
             type="email"
             data-testid="email-input"
@@ -30,6 +43,18 @@ class Login extends React.Component {
             onChange={ this.handleChange }
           />
         </label>
+        <label htmlFor="passwordId">
+          Password:
+          <input
+            type="password"
+            data-testid="password-input"
+            id="passwordId"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="button" disabled={ disabled }> Entrar </button>
       </div>);
   }
 }
