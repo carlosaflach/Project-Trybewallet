bb633d490fcddb6dfbbbf56dc858c011e5e6a907
Carlos Afonso <carlos.flach15@gmail.com>
2021-11-22 14:58:49 -0300

    Creates email input.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email } = this.state;
+    return (
+      <div>
+        <label htmlFor="emailId">
+          <input
+            type="email"
+            data-testid="email-input"
+            id="emailId"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </div>);
   }
 }
 
