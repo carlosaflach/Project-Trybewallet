e4a3adb36178238c38980e62f4e11b98d8818f68
Carlos Afonso <carlos.flach15@gmail.com>
2021-11-28 17:41:57 -0300

    Fix css problems for Table Component.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
 import ExpensesForm from './ExpensesForm';
-import '../CSS/wallet.css';
 
 class Wallet extends React.Component {
   render() {
